4dc12b56de49d2b6c04ea4d7e65d03179fb7f6af
Luisc00 <luisc.moralesc@uqvirtual.edu.co>
2024-10-23 19:55:13 -0500

    Adelanto formularios

--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -1,12 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './registro.component.html',
-  styleUrl: './registro.component.css'
+  styleUrls: ['./registro.component.css'] 
 })
 export class RegistroComponent {
+  registroForm!: FormGroup; 
 
+  constructor(private formBuilder: FormBuilder) {
+    this.crearFormulario(); 
+  }
+  
+  public registrar() {
+    console.log(this.registroForm.value);
+ }
+ passwordsMatchValidator(formGroup: FormGroup) {
+  const password = formGroup.get('password')?.value;
+  const confirmaPassword = formGroup.get('confirmaPassword')?.value;
+  return password == confirmaPassword ? null : { passwordsMismatch: true };
+ }
+ 
+ 
+
+  private crearFormulario() {
+    this.registroForm = this.formBuilder.group({
+      cedula: ['', [Validators.required]],
+      nombre: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      direccion: ['', [Validators.required]],
+      telefono: ['', [Validators.required, Validators.maxLength(10)]],
+      password: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(30)]]
+    },
+    { validators: this.passwordsMatchValidator } as AbstractControlOptions
+  );
 }
+}
+
+
