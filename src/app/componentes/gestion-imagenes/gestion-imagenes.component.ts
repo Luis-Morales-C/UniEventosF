abdcaf752181b07a1e4be81308690ffdb12caa5b
Luisc00 <luisc.moralesc@uqvirtual.edu.co>
2024-11-01 20:46:48 -0500

    Adelanto login y gestion de imagenes

---
+++ src/app/componentes/gestion-imagenes/gestion-imagenes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-imagenes',
+  standalone: true,
+  imports: [],
+  templateUrl: './gestion-imagenes.component.html',
+  styleUrl: './gestion-imagenes.component.css'
+})
+export class GestionImagenesComponent {
+
+}
--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.crearFormulario();
+  }
+
+  private crearFormulario() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(10)]]
+    });
+  }
+
+  public iniciarSesion() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
+  }
 }
